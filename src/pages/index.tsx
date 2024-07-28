ef3726fd1f4899801b7bcf58af049e01d46f0067
Du Jiajun <jiajun_du@outlook.com>
2024-07-28 17:52:32 +0800

    feat: remove recommend page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,5 @@
-import { Card, Flex } from "antd";
-
-import BaseCourseItem from "../components/base-course-item";
-import CourseItem from "../components/course-item";
-import RateInfoDetail from "../components/rate-info-detail";
-import ReviewInCourseFilter from "../components/review-in-course-filter";
-import ReviewItem from "../components/review-item";
-import TeacherItem from "../components/teacher-item";
-import TrainingPlanFilter from "../components/training-plan-filter";
-import TrainingPlanItem from "../components/training-plan-item";
-import {
-  baseCourseDetail,
-  courseListItem,
-  rateInfo,
-  reviewListItem,
-  teacherListItem,
-  trainingPlanListItem,
-} from "../models/mock";
-
 const IndexPage = () => {
-  return (
-    <Flex gap={20} vertical>
-      <Flex wrap gap={20}>
-        <BaseCourseItem baseCourse={baseCourseDetail}></BaseCourseItem>
-        <CourseItem course={courseListItem}></CourseItem>
-        <TeacherItem teacher={teacherListItem}></TeacherItem>
-        <TrainingPlanItem
-          trainingPlan={trainingPlanListItem}
-        ></TrainingPlanItem>
-        <ReviewItem review={reviewListItem} showCourse></ReviewItem>
-      </Flex>
-      <Flex wrap gap={20}>
-        <Card>
-          <ReviewInCourseFilter></ReviewInCourseFilter>
-        </Card>
-        <Card>
-          <TrainingPlanFilter></TrainingPlanFilter>
-        </Card>
-      </Flex>
-      <Flex wrap gap={20}>
-        <Card>
-          <RateInfoDetail rateInfo={rateInfo}></RateInfoDetail>
-        </Card>
-      </Flex>
-    </Flex>
-  );
+  return <></>;
 };
 
 export default IndexPage;
