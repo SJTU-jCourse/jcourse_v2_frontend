d729a0be48b5b4917433e2cb47fe63e110e858a1
Du Jiajun <jiajun_du@outlook.com>
2024-07-29 21:46:49 +0800

    fix: style

--- src/components/user-detail-card.tsx
+++ src/components/user-detail-card.tsx
@@ -5,21 +5,19 @@ import { UserDetailProps } from "../models/model";
 const UserDetailCard = ({ user }: { user: UserDetailProps }) => {
   return (
     <Space direction="vertical">
-      <Row gutter={[16, 16]}>
+      <Row gutter={[16, 16]} wrap={false}>
         <Col>
           <Avatar src={user.avatar} size={64}></Avatar>
         </Col>
         <Col>
-          <div>
-            <Typography.Text style={{ fontSize: 22, fontWeight: 600 }}>
-              {user.username}
-            </Typography.Text>
-          </div>
-          <div>
-            <Typography.Text>{user.bio}</Typography.Text>
-          </div>
+          <Typography.Text style={{ fontSize: 22, fontWeight: 600 }}>
+            {user.username}
+          </Typography.Text>
         </Col>
       </Row>
+      <div>
+        <Typography.Text>{user.bio}</Typography.Text>
+      </div>
     </Space>
   );
 };
